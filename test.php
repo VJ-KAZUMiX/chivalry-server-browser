a573894deac2918bd842659229fab0e9461062fb
KAZUMiX <kazumix@gmail.com>
2013-03-17 15:39:46 +0900

    コミットテスト
    とりあえず test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: KAZUMiX
+ * Date: 13/03/06
+ * Time: 0:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+phpinfo();
