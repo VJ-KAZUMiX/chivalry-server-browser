d7bde075c8582dbf16d1b1f9d1325ae621b17858
KAZUMiX <kazumix@gmail.com>
2013-03-25 02:18:51 +0900

    edited

--- GameServerManager.php
+++ GameServerManager.php
@@ -7,6 +7,38 @@
  * To change this template use File | Settings | File Templates.
  */
 
+require_once "steam-condenser-php/lib/steam-condenser.php";
+require_once "GeoIP/php-1.12/geoip.inc";
+
+/**
+ * Class GameServerManager
+ */
 class GameServerManager {
 
+    /**
+     * @var GameServerManager
+     */
+    private static $sharedInstance = null;
+
+    /**
+     * @return GameServerManager
+     */
+    public static function sharedManager() {
+        if (!self::$sharedInstance) {
+            self::$sharedInstance = new self();
+        }
+        return self::$sharedInstance;
+    }
+
+    private static $sqlConnection = null;
+
+    private static function getSqlConnection() {
+        if (!self::$sqlConnection) {
+
+        }
+    }
+
+    private function __construct() {
+
+    }
 }
